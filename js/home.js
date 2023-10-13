01d866ee20d2d7f45ddc2a2da10cebb9158f88c0
Lokteeva <swsw06@mail.ru>
2023-10-13 11:44:29 +0300

    home hover

--- js/home.js
+++ js/home.js
@@ -1,2 +1,44 @@
-const prompt = document.querySelector('.home__socials-telegram-prompt');
-console.log(prompt)
+//Telegram
+const promptTelegram = document.querySelector('.home__socials-telegram-prompt');
+const btnTelegram = document.querySelector('.home__socials-telegram');
+
+btnTelegram.addEventListener('mouseover', function(){
+  promptTelegram.classList.remove('hidden');
+})
+
+btnTelegram.addEventListener('mouseout', function(){
+  promptTelegram.classList.add('hidden');
+})
+//Whatsapp
+const promptWhatsapp = document.querySelector('.home__socials-whatsapp-prompt');
+const btnWhatsapp = document.querySelector('.whatsapp__link');
+
+btnWhatsapp.addEventListener('mouseover', function(){
+  promptWhatsapp.classList.remove('hidden');
+})
+
+btnWhatsapp.addEventListener('mouseout', function(){
+  promptWhatsapp.classList.add('hidden');
+})
+//mail
+const promptSms = document.querySelector('.home__socials-sms-prompt');
+const btnSms = document.querySelector('.home__socials-sms');
+
+btnSms.addEventListener('mouseover', function(){
+  promptSms.classList.remove('hidden');
+})
+
+btnSms.addEventListener('mouseout', function(){
+  promptSms.classList.add('hidden');
+})
+//message
+const promptMessage = document.querySelector('.home__socials-message-prompt');
+const btnMessage = document.querySelector('.home__socials-message');
+
+btnMessage.addEventListener('mouseover', function(){
+  promptMessage.classList.remove('hidden');
+})
+
+btnMessage.addEventListener('mouseout', function(){
+  promptMessage.classList.add('hidden');
+})
