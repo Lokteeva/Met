e10cdcbb8f36af4acedffbf7fac81d4e2cb16aec
Lokteeva <swsw06@mail.ru>
2023-10-15 09:46:12 +0300

    production hover

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,6 @@
 
 const owl = $('.owl-carousel');
 owl.owlCarousel({
-  // center: true,
   loop: true,
   startPosition: 1,
   items: 3,
@@ -14,6 +13,18 @@ $('.carousel__btn-left').click(function () {
   owl.trigger('prev.owl.carousel');
 });
 
-
-
+//2 production
+const owl2 = $('.owl-carousel');
+owl2.owlCarousel({
+  loop: true,
+  startPosition: 1,
+  items: 3,
+  margin: 0, 
+});
+$('.carousel2__btn-right').click(function () {
+  owl2.trigger('next.owl.carousel');
+});
+$('.carousel2__btn-left').click(function () {
+  owl2.trigger('prev.owl.carousel');
+});
 
