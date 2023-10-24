5f2fc66aaee4158eaadba025a05f5e60f9d206d0
Lokteeva <swsw06@mail.ru>
2023-10-24 18:45:24 +0300

    ispravleniya 1

--- js/home.js
+++ js/home.js
@@ -91,3 +91,43 @@ function reveal() {
   }
 }
 window.addEventListener("scroll", reveal);
+
+
+// active class of menu items onscroll
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+
+	if (window.innerWidth > 768) {
+		document.querySelectorAll('.section').forEach((el, i) => {
+			if (el.offsetTop - document.querySelector('.home__nav').clientHeight <= scrollDistance) {
+				document.querySelectorAll('.home__nav a').forEach((el) => {
+					if (el.classList.contains('active')) {
+						el.classList.remove('active');
+					}
+				});
+
+				document.querySelectorAll('.home__nav li')[i].querySelector('a').classList.add('active');
+			}
+		});
+	}
+});
+
+//
+
+// window.addEventListener('scroll', () => {
+// 	let scrollDistance = window.scrollY;
+
+// 	if (window.innerWidth > 768) {
+// 		document.querySelectorAll('.section').forEach((el, i) => {
+// 			if (el.offsetTop - document.querySelector('.nav').clientHeight <= scrollDistance) {
+// 				document.querySelectorAll('.nav a').forEach((el) => {
+// 					if (el.classList.contains('active')) {
+// 						el.classList.remove('active');
+// 					}
+// 				});
+
+// 				document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('active');
+// 			}
+// 		});
+// 	}
+// });
